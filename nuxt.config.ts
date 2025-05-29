35f07518e2db5275d2282e4b96be9981e722f873
Stevn <117241570+stevnHS@users.noreply.github.com>
2025-05-30 01:19:00 +0800

    Nt 10 (#50)
    * :sparkles: add TailwindCSS V4
    
    * daisyUI, dark mode
    
    * nuxt/icon 基於 Iconify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint"],
+  modules: ["@nuxt/eslint", "@nuxt/icon"],
+  css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 });
