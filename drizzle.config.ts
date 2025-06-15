bdee1d7f9ac5f2dbea7d3e0db15820e347302b84
stevn <stevnliao@gmail.com>
2025-06-16 01:06:43 +0800

    資料庫migration(drizzle,terso)

---
+++ drizzle.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "drizzle-kit";
+
+import env from "./lib/env";
+
+export default defineConfig({
+  out: "./lib/db/migrations",
+  schema: "./lib/db/schema/index.ts",
+  dialect: "turso",
+  dbCredentials: {
+    url: env.TURSO_DATABASE_URL,
+    authToken: env.NODE_ENV === "development" ? undefined : env.TURSO_AUTH_TOKEN,
+  },
+  casing: "snake_case",
+
+});
